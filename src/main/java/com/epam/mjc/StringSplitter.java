49bc02a3fd077335bf8983f768600ea4d2cfe807
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 14:35:20 +0600

    Update StringSplitter.java

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -13,31 +13,17 @@ public class StringSplitter {
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
-    public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        List<String> substrings = new ArrayList<>();
-        StringBuilder currentSubstring = new StringBuilder();
-        for (char c : source.toCharArray()) {
-            boolean delimiterFound = false;
-
-            for (String delimiter : delimiters) {
-                if (delimiter.indexOf(c) != -1) {
-                    if (currentSubstring.length() > 0) {
-                      
-                        substrings.add(currentSubstring.toString()); 
-                        currentSubstring.setLength(0);
-                    }
-                    delimiterFound = true;
-                    break;
-                }
-            }
-
-            if (!delimiterFound) {
-                currentSubstring.append(c);
+    public List<String> splitByDelimiters(String input, List<String> delimiters) {
+        String regex = "[" + String.join("", delimiters) + "]";
+        String[] tokens = input.split(regex);
+        
+        List<String> result = new ArrayList<>();
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                result.add(token);
             }
         }
-        if (currentSubstring.length() > 0) {
-            substrings.add(currentSubstring.toString());
-        }
-        return substrings;
+        
+        return result;
     }
 }
