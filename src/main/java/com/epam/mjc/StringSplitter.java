327a2c022b517b8348646a5b3587147812f84256
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 12:29:32 +0600

    Update StringSplitter.java

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -13,6 +13,30 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> substrings = new ArrayList<>();
+        StringBuilder currentSubstring = new StringBuilder();
+        for (char c : source.toCharArray()) {
+            boolean delimiterFound = false;
+
+            for (String delimiter : delimiters) {
+                if (delimiter.indexOf(c) != -1) {
+                    if (currentSubstring.length() > 0) {
+                      
+                        substrings.add(currentSubstring.toString()); 
+                        currentSubstring.setLength(0);
+                    }
+                    delimiterFound = true;
+                    break;
+                }
+            }
+
+            if (!delimiterFound) {
+                currentSubstring.append(c);
+            }
+        }
+        if (currentSubstring.length() > 0) {
+            substrings.add(currentSubstring.toString());
+        }
+        return substrings;
     }
 }
